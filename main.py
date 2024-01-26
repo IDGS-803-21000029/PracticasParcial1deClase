5aa15d003db08b329d0de5e24edb8b3cbb0df0c2
IDGS-803-21000029 <21000029@alumnos.utleon.edu.mx>
2024-01-26 17:51:45 -0600

    JIGC 26.01.2023 Actividad Boletos Descuento

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
+# Raiz
 @app.route("/")
 def index():
     escuela = "UTL"
@@ -10,6 +10,7 @@ def index():
 
     return render_template("index.html", escuela=escuela, alumnos=alumnos)
 
+# Practica de Operaciones
 @app.route("/formulario")
 def form():
     return render_template("formOperaciones.html")
@@ -30,7 +31,34 @@ def operaciones():
         return "<h1>La division es: {}</h1>".format(str(int(num1) / int(num2)))
     else:
         return "<h1>No se ha podido identificar la operacion</h1>"
+    
+# CINEPOLIS
+@app.route("/cinepolis", methods=["POST", "GET"])
+def cine():
+    if request.method == "GET":
+        return render_template("cinepolis.html", total=0, nombre="")
+    else:
+        nombre = request.form.get("txtNombre")
+        cantidadCompradores = request.form.get("txtCantCompradores")
+        tarjeta = request.form.get("card")
+        cantidadBoletos = request.form.get("txtCantBoletos")
 
+        if int(cantidadBoletos) > int(cantidadCompradores) * 7:
+            return render_template("cinepolis.html", mensaje="No se pueden vender mas de 7 boletos por persona, vuelva a intentarlo", total=0)
+        else:
+            valorSubtotal = float(cantidadBoletos) * 12.000
+        
+        if int(cantidadBoletos) > 5:
+            valorTotal = valorSubtotal - (valorSubtotal * 0.15)
+        elif int(cantidadBoletos) > 2:
+            valorTotal = valorSubtotal - (valorSubtotal * 0.10)
+        else:
+            valorTotal = valorSubtotal
+
+        if tarjeta == "1":
+            valorTotal = valorTotal - (valorTotal * 0.10)
+
+        return render_template("cinepolis.html", total=float(valorTotal), nombre=str(nombre))
 
 if __name__ == "__main__":
     app.run(debug=True)
